e4225063de48833bf637edf8e5c05079efc761c0
Sarsey Walker <sarsey.james@gmail.com>
2022-07-18 20:38:08 -0300

    :bento: Added routes
    Added router-outlet to alunos e professores

--- SmartSchool/src/app/app-routing.module.ts
+++ SmartSchool/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AlunosComponent } from './alunos/alunos.component';
+import { ProfessoresComponent } from './professores/professores.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'professores', component: ProfessoresComponent},
+  { path:'alunos', component: AlunosComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
