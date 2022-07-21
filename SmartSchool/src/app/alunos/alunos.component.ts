cf94fb813c46770233486b77c000438353724f96
Sarsey Walker <sarsey.james@gmail.com>
2022-07-20 21:38:04 -0300

    :construction: Added table of data alunos and professores

--- SmartSchool/src/app/alunos/alunos.component.ts
+++ SmartSchool/src/app/alunos/alunos.component.ts
@@ -10,13 +10,13 @@ export class AlunosComponent implements OnInit {
   titulo = 'Alunos'
 
   alunos = [
-      {nome: 'Marta'},
-      {nome: 'Miguel'},
-      {nome: 'João'},
-      {nome: 'Marcos'},
-      {nome: 'Paula'},
-      {nome: 'Ana'},
-      {nome: 'Júlia'},
+      {id: 1, nome: 'Marta', sobrenome:'Rofrigues', telefone:'99123456789'},
+      {id: 2, nome: 'Miguel', sobrenome:'Palhares', telefone:'99123456777'},
+      {id: 3, nome: 'João', sobrenome:'Vitor', telefone:'99123456999'},
+      {id: 4, nome: 'Marcos', sobrenome:'Pontes', telefone:'99123456987'},
+      {id: 5, nome: 'Paula', sobrenome:'Rodrigues', telefone:'99123456014'},
+      {id: 6, nome: 'Ana', sobrenome:'Paula', telefone:'99123456321'},
+      {id: 7, nome: 'Júlia', sobrenome:'Neves', telefone:'99123456712'},
   ]
 
   constructor() { }
--- SmartSchool/src/app/professores/professores.component.ts
+++ SmartSchool/src/app/professores/professores.component.ts
@@ -9,12 +9,12 @@ export class ProfessoresComponent implements OnInit {
 
   titulo = 'Professores'
   professores = [
-    {nome: 'Paulo'},
-    {nome: 'Jacira'},
-    {nome: 'Ellenna'},
-    {nome: 'Sergio'},
-    {nome: 'Onofre'},
-    {nome: 'Ricardo'}
+    {id: 1, nome: 'Paulo', disciplina:'Português'},
+    {id: 2, nome: 'Jacira', disciplina:'Português'},
+    {id: 3, nome: 'Ellenna', disciplina:'História'},
+    {id: 4, nome: 'Sergio', disciplina:'Geografia'},
+    {id: 5, nome: 'Onofre', disciplina:'Português'},
+    {id: 6, nome: 'Ricardo', disciplina:'Matemática'}
   ]
   constructor() { }
 
